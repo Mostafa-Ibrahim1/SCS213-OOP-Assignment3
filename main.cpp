233b19d142a5fb340ddfa5cfd05ebd2488521d07
Youssef Magdi <youssefmagdisalah@gmail.com>
2021-12-19 10:41:26 +0200

    Added Classes Declaration

--- main.cpp
+++ main.cpp
@@ -3,6 +3,45 @@
 
 using namespace std;
 
-int main(){
+class FloatArray {
+private:
+    float *floats;
+    int arraySize;
+public:
+    FloatArray(int);
+    virtual void add(float);
+    //ofstream overloading
+    //ifstream overloading
+    ~FloatArray();
+};
+
+class SortedArray : public FloatArray {
+public:
+    SortedArray(int);
+    void add(float);
+};
+
+class FrontArray : public FloatArray {
+public:
+    FrontArray(int);
+    void add(float);
+};
+
+class PositiveArray : public FrontArray {
+public:
+    PositiveArray(int);
+    void add(float);
+};
+
+class NegativeArray : public FrontArray {
+public:
+    NegativeArray(int);
+    void add(float);
+};
+
+
+int main() {
+    cout << "Enter the name of the input file:" << endl;
+    cout << "Enter the name of the output file:" << endl;
     return 0;
 }
