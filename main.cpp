ab60cf566f20485ff3d57406fb7a51eb3b9f69cf
Mostafa-Ibrahim1 <78238174+Mostafa-Ibrahim1@users.noreply.github.com>
2021-12-21 17:05:18 +0200

    Revert "Fix: Input & Output Files"
    This reverts commit 2c5d60e0412f8ba2dd0d8a4e7ab0a5677178549f.

--- main.cpp
+++ main.cpp
@@ -139,11 +139,9 @@ void operator>>(ifstream& inFile,FloatArray** ArrayOfPointers){
     string type;
     float element;
     FloatArray* pointerToObject;
-    // while(!inFile.eof()){
+    while(!inFile.eof()){
         for(int i=0;i<10;i++){
-            if(inFile.eof()){
-                break;
-            }
+
             inFile >> type;
             inFile >> size;
             if(type == "Array"){
@@ -173,7 +171,7 @@ void operator>>(ifstream& inFile,FloatArray** ArrayOfPointers){
 
         pointerToObject = 0;
         }
-    // }
+    }
 
 }
 
@@ -182,9 +180,9 @@ void operator>>(ifstream& inFile,FloatArray** ArrayOfPointers){
 ofstream& operator<<(ofstream& outFile, FloatArray** ArrayOfPointers){
 
     for(int i = 0; i < 10; i++){
-        outFile << ArrayOfPointers[i]->arraySize_ << "|\t";
+        outFile << ArrayOfPointers[i]->arraySize_ << "|   ";
         for(int j = 0; j < ArrayOfPointers[i]->arraySize_; j++){
-                outFile << ArrayOfPointers[i]->floats_[j] << "\t";
+                outFile << ArrayOfPointers[i]->floats_[j] << "   ";
         }
         outFile << "\n";
     }
